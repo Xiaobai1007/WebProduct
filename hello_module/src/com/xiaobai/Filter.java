8eeaa6505261374797b98cd061e6f58ac5839883
Xiaobai1007 <849494330@qq.com>
2018-08-10 08:33:37 +0800

    Web项目

---
+++ hello_module/src/com/xiaobai/Filter.java
@@ -0,0 +1,17 @@
+package com.xiaobai;
+
+import java.io.IOException;
+
+public class Filter implements javax.servlet.Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(javax.servlet.ServletRequest req, javax.servlet.ServletResponse resp, javax.servlet.FilterChain chain) throws javax.servlet.ServletException, IOException {
+        chain.doFilter(req, resp);
+    }
+
+    public void init(javax.servlet.FilterConfig config) throws javax.servlet.ServletException {
+
+    }
+
+}
